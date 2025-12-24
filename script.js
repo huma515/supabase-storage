0b03c7e993e2eff04231a96f0df3e5db4a4a232c
huma515 <humahashim11@gmail.com>
2025-12-25 02:00:54 +0500

    supabase storage completed

--- script.js
+++ script.js
@@ -3,34 +3,70 @@ import supabase from "./config.js"
 
 let imgUp = document.getElementById("imgInp")
 let btn = document.getElementById("btn")
+let main = document.getElementById("main")
 
 let picName;
 
 async function uploadF(e) {
   e.preventDefault()
- 
-let fileName = imgUp.files[0].name
-let file = imgUp.files[0]
+
+  let fileName = imgUp.files[0].name
+  let file = imgUp.files[0]
+
+  const { data, error } = await supabase
+    .storage
+    .from('myfile')
+    .upload(fileName, file,)
+
+  if (data) {
+
+    picName = data.path
+    console.log("upload successfully", picName)
+
+    const { data: urlData } = supabase
+      .storage
+      .from('myfile')
+      .getPublicUrl(picName)
+
+    if (urlData) {
+      console.log(urlData)
+      console.log(urlData.publicUrl)
+
+      const { error } = await supabase
+  .from('image')
+  .insert({ url: urlData.publicUrl})
+  getPic() 
+    }
+  }
+}
+btn.addEventListener("click", uploadF)
+
+
+async function getPic() {
+  
+
+  try {
+    
+
 
 const { data, error } = await supabase
-  .storage
-  .from('myfile')
-  .upload( fileName, file,)
+  .from('image')
+  .select("*")
+   data.forEach(pic => {
 
-  if(data){
+    main.innerHTML += `
+    <div style="display:inline-block; margin:5px;">
+    <img src='${pic.url}' style="width:100%; max-width:200px; height:auto; border-radius:8px;">
+  </div>
+    
+    `
+   });
 
-picName = data.path
-console.log(  "upload successfully"     , picName)
 
+
+  } catch (error) {
+    console.log(error)
   }
-
-  else {
-    console.log("Upload error:", error)
-  }
-   
- 
-
-
-
 }
-btn.addEventListener("click" , uploadF)
+
+ 
